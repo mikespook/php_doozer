db0a0beb0dd3b91f5e050f57e0501cb140ee7be3
mikespook <mikespook@gmail.com>
2012-12-13 18:04:42 +0800

    All other doozer commands

--- doozer/client.h
+++ doozer/client.h
@@ -10,10 +10,12 @@ extern int doozer_current_revision(int, int64_t *);
 extern int doozer_set(int, char *, uint8_t *, size_t, int64_t *);
 extern int doozer_get(int, char *, int64_t, uint8_t **, size_t *);
 extern int doozer_delete(int, char *, int64_t);
+extern int doozer_dir(int, char *, int64_t, int32_t, char **);
+extern int doozer_stat(int, char *, int64_t *, int32_t *);
+extern int doozer_access(int, char *);
+extern int doozer_wait(int, char *, int64_t *, 
+        char **, char **, size_t *, char *);
 /*
-extern void doozer_dir();
-extern void doozer_stat();
-extern void doozer_access();
 extern void doozer_walk();
 extern void doozer_hosts();
 extern void doozer_wait();
